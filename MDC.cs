765315cf769c2e2b7cddbfa533f1e2b7f70360ea
Camila Zelenovsky <camila_zele@hotmail.com>
2020-08-05 22:14:44 -0300

    first commit

---
+++ MDC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex6
+{
+    public static class MDC
+    {
+        public static int Mdc (int a, int b)
+        {
+            int remain = 1;
+
+            while (remain != 0)
+            {
+                remain = a % b;
+                a = b;
+                b = remain;
+            }
+            return a;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ex6
+{
+    class Program
+    {
+        public static object MaxValue { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iremos calcura o Maior Divisor Comum!");
+            Console.WriteLine("Qual o primeiro número?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual o segundo número?");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int retorno = MDC.Mdc(a, b);
+
+            Console.WriteLine($"O Maior Divisor comum entre {a} e {b} é {retorno}.");
+        }
+    }
+}
